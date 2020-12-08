b288f12075598fc44a28077cb71fb4903383c3b1
Yaroslav Ryabov <yar.ryabov@mail.ru>
2020-12-09 00:51:56 +0400

    init project

---
+++ src/main/java/ru/webinari/WebinariApplication.java
@@ -0,0 +1,13 @@
+package ru.webinari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebinariApplication.class, args);
+    }
+
+}
