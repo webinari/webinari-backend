ced2047c8eb55e3a97738c87cb7828a57a436132
Yaroslav Ryabov <yar.ryabov@mail.ru>
2021-01-20 04:48:51 +0400

    add room controller

---
+++ src/main/java/ru/webinari/dashboard/room/RoomController.java
@@ -0,0 +1,16 @@
+package ru.webinari.dashboard.room;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomController {
+
+    @GetMapping
+    public String getRooms() {
+        return "done";
+    }
+
+}
