db6e3a42d843c8fc4a50e47129638239a59fc255
Yaroslav Ryabov <yar.ryabov@mail.ru>
2021-01-20 06:43:08 +0400

    remove roles template

--- src/main/resources/db/migration/V1__base.sql
+++ src/main/resources/db/migration/V1__base.sql
@@ -1,13 +1,6 @@
-create table roles
-(
-    id   bigserial primary key,
-    name varchar unique not null
-);
-
 create table users
 (
     id       bigserial primary key,
---     role_id  bigint references roles (id) not null,
     username varchar unique               not null,
     password varchar                      not null,
     email    varchar                      not null
