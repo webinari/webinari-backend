c04b75e00f87b03c8cd69a0c2b65448f18b4b4fb
Yaroslav Ryabov <yar.ryabov@mail.ru>
2021-02-26 18:50:19 +0400

    delete event

--- src/main/java/ru/webinari/web/core/event/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.webinari.web.core.event.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.webinari.web.core.event.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
